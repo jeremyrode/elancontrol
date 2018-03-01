3ed603942b54d689b3534c7184fa6e9620c8c6d5
Jeremy Rode <jprode@gmail.com>
2018-03-01 02:56:10 +0000

    send_zpad_command working and tested

--- send_zpad_command.c
+++ send_zpad_command.c
@@ -1,104 +1,133 @@
-// Sent Arbitrary ZPAD Commands
+//  Bit-bang Elan S6 Command Seuquence on GPI10
+//  Uses: How to access GPIO registers from C-code on the Raspberry-Pi
+//  Dom and Gert
+
 #define BCM2708_PERI_BASE        0x3F000000
 #define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
-
+  
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <unistd.h>
 #include <time.h>
-
+ 
 #define PAGE_SIZE (4*1024)
 #define BLOCK_SIZE (4*1024)
-// Delays from measured commands
-#define PULSE_WIDTH_DELAY 906
-#define NARROW (const struct timespec[]){{0,5069000L}}, NULL
-#define WIDE (const struct timespec[]){{0,7064000L}}, NULL
-// Which Pin
-#define GPIO_PIN 1<<10
-
+ 
+int  mem_fd;
+void *gpio_map;
+ 
+// I/O access
+volatile unsigned *gpio;
+ 
 // GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
 #define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
 #define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
 #define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
-
+ 
 #define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
 #define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
-
+ 
 #define GET_GPIO(g) (*(gpio+13)&(1<<g)) // 0 if LOW, (1<<g) if HIGH
-
+ 
 #define GPIO_PULL *(gpio+37) // Pull up/pull down
 #define GPIO_PULLCLK0 *(gpio+38) // Pull up/pull down clock
-
+ 
 void setup_io();
-
-int main(int argc, char *argv[])
+ 
+int main(int argc, char **argv)
 {
-    int g,rep,code;
-    
-    if (argc > 2)
-        return -1;
-    code = atoi(argv[1]);
-    // Set up gpi pointer for direct register access
-    setup_io()
-    // Set GPIO pins 7-11 to output
-    for (g=7; g<=11; g++)
+  int g,rep,code;
+ 
+  if (argc<2)
+  {
+	  printf("Need an integer command");
+	  return -1;
+  }
+  code = atoi(argv[1]);
+  // Set up gpi pointer for direct register access
+  setup_io();
+ 
+  // Set GPIO pins 7-11 to output
+  for (g=7; g<=11; g++)
+  {
+    INP_GPIO(g); // must use INP_GPIO before we can use OUT_GPIO
+    OUT_GPIO(g);
+  }
+
+  for (rep=0; rep<5; rep++)
+  {
+    GPIO_SET = 1<<10;
+    g=0;
+    while (g<906)
     {
-        INP_GPIO(g); // must use INP_GPIO before we can use OUT_GPIO
-        OUT_GPIO(g);
+      g++;
     }
-    for (rep=0; rep<6; rep++) //Send the preamble (all shorts)
+    GPIO_CLR = 1<<10;
+    nanosleep((const struct timespec[]){{0,5000000L}}, NULL);
+  }
+ for (rep=0; rep<6; rep++)
+  {
+    GPIO_SET = 1<<10;
+    g=0;
+    while (g<906)
     {
-        GPIO_SET = GPIO_PIN;
-        g=0;
-        while (g<PULSE_WIDTH_DELAY)
-            g++;
-        GPIO_CLR = GPIO_PIN;
-        nanosleep(NARROW);
+      g++;
     }
-    for (rep=0; rep<6; rep++) //Send the code
+    GPIO_CLR = 1<<10;
+    if (code & 1<<5-rep)
     {
-        GPIO_SET = GPIO_PIN;
-        g=0;
-        while (g<PULSE_WIDTH_DELAY)
-            g++;
-        GPIO_CLR = GPIO_PIN;
-        if (code & 1<<rep)
-            nanosleep(WIDE);
-        else
-            nanosleep(NARROW);
+      nanosleep((const struct timespec[]){{0,7000000L}}, NULL);
     }
-    return 0;
-}
+    else
+    {
+      nanosleep((const struct timespec[]){{0,5000000L}}, NULL);
+    }
+  }
+
+  GPIO_SET = 1<<10;
+  g=0;
+  while (g<906)
+  {
+    g++;
+  }
+  GPIO_CLR = 1<<10;
+
+  return 0;
+ 
+} // main
+ 
 //
 // Set up a memory regions to access GPIO
 //
 void setup_io()
 {
-    int  mem_fd;
-    void *gpio_map;
-// I/O access
-    volatile unsigned *gpio;
-    /* open /dev/mem */
-    if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
-        printf("can't open /dev/mem \n");
-        exit(-1);
-    }
-    /* mmap GPIO */
-    gpio_map = mmap(
-            NULL,             //Any adddress in our space will do
-            BLOCK_SIZE,       //Map length
-            PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
-            MAP_SHARED,       //Shared with other processes
-            mem_fd,           //File to map
-            GPIO_BASE         //Offset to GPIO peripheral
-            );
-    close(mem_fd); //No need to keep mem_fd open after mmap
-    if (gpio_map == MAP_FAILED) {
-        printf("mmap error %d\n", (int)gpio_map);//errno also set!
-        exit(-1);
-    }
-    // Always use volatile pointer!
-    gpio = (volatile unsigned *)gpio_map;
+   /* open /dev/mem */
+   if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
+      printf("can't open /dev/mem \n");
+      exit(-1);
+   }
+ 
+   /* mmap GPIO */
+   gpio_map = mmap(
+      NULL,             //Any adddress in our space will do
+      BLOCK_SIZE,       //Map length
+      PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
+      MAP_SHARED,       //Shared with other processes
+      mem_fd,           //File to map
+      GPIO_BASE         //Offset to GPIO peripheral
+   );
+ 
+   close(mem_fd); //No need to keep mem_fd open after mmap
+ 
+   if (gpio_map == MAP_FAILED) {
+      printf("mmap error %d\n", (int)gpio_map);//errno also set!
+      exit(-1);
+   }
+ 
+   // Always use volatile pointer!
+   gpio = (volatile unsigned *)gpio_map;
+ 
+ 
 } // setup_io
