45bd653c59c06d19f08da0c61c5603615cc23117
Jeremy Rode <jprode@gmail.com>
2018-03-02 11:08:18 -0800

    Breaking recieve into bytes

--- command_server.cpp
+++ command_server.cpp
@@ -2,39 +2,43 @@
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
 #include <boost/lexical_cast.hpp>
+#include "send_zpad_command.h"
 
 using boost::asio::ip::udp;
 
 int main(int argc, char* argv[])
 {
-  try
-  {
-    if (argc != 2)
+    try
     {
-	    std::cerr << "IP adress" << std::endl;
-	    return 1;
+        if (argc != 2)
+        {
+            std::cerr << "IP adress" << std::endl;
+            return 1;
+        }
+        boost::asio::io_service io_service;
+        
+        udp::endpoint local_endpoint = boost::asio::ip::udp::endpoint(
+                boost::asio::ip::address::from_string(argv[1]), 6969);
+        std::cout << "Local bind " << local_endpoint << std::endl;
+        
+        udp::socket socket(io_service, local_endpoint);
+        while (1)
+        {
+            boost::array<char, 128> recv_buf;
+            udp::endpoint sender_endpoint;
+            //std::cout << "About to RX" << std::endl;
+            size_t len = socket.receive_from(boost::asio::buffer(recv_buf), sender_endpoint);
+            for (int x=0; x<len; x++)
+            {
+                std::cout.write(recv_buf[x].data(), 1);
+                std::cout << std::endl;
+            }
+        }
     }
-    boost::asio::io_service io_service;
-
-    udp::endpoint local_endpoint = boost::asio::ip::udp::endpoint(
-	  boost::asio::ip::address::from_string(argv[1]), 6969);
-    std::cout << "Local bind " << local_endpoint << std::endl;
- 
-    udp::socket socket(io_service, local_endpoint); 
-    while (1)
+    catch (std::exception& e)
     {
-    boost::array<char, 128> recv_buf;
-    udp::endpoint sender_endpoint;
-    //std::cout << "About to RX" << std::endl;
-    size_t len = socket.receive_from(boost::asio::buffer(recv_buf), sender_endpoint);
-    std::cout.write(recv_buf.data(), len);
-    std::cout << std::endl;
+        std::cerr << e.what() << std::endl;
     }
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-  }
-
-  return 0;
+    
+    return 0;
 }
