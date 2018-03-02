be60067193880d01b5683512c14adc2aa2daad21
Jeremy Rode <jprode@gmail.com>
2018-03-01 17:01:40 -0800

    udp handler

--- command_server.cpp
+++ command_server.cpp
@@ -1,30 +1,40 @@
-
 #include <iostream>
-#include <string>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
+#include <boost/lexical_cast.hpp>
 
 using boost::asio::ip::udp;
 
-int main()
+int main(int argc, char* argv[])
 {
   try
   {
+
     boost::asio::io_service io_service;
-    udp::socket socket(io_service, udp::endpoint(udp::v4(), 6969));
 
-    for (;;)
-    {
-      boost::array<char, 1> recv_buf;
-      udp::endpoint remote_endpoint;
-      boost::system::error_code error;
-      socket.receive_from(boost::asio::buffer(recv_buf),
-          remote_endpoint, 0, error);
-      std::cout.write(&recv_buf[0],recv_buf.size());
+    udp::endpoint local_endpoint = boost::asio::ip::udp::endpoint(
+	  127.0.0.1, 6969);
+    std::cout << "Local bind " << local_endpoint << std::endl;
+ 
+// MODE 1: WORKS  
+    udp::socket socket(io_service, local_endpoint);
 
-      if (error && error != boost::asio::error::message_size)
-        throw boost::system::system_error(error);
-    }
+// MODE 2: WORKS
+//    udp::socket socket(io_service, udp::endpoint(udp::v4(), boost::lexical_cast<int>(argv[2]) )),
+
+// MODE 3: WORKS
+//    udp::socket socket(io_service);
+//    socket.open(udp::v4());
+//    socket.bind(local_endpoint);
+///////////////
+
+    boost::array<char, 128> recv_buf;
+    
+    udp::endpoint sender_endpoint;
+    size_t len = socket.receive_from(
+        boost::asio::buffer(recv_buf), sender_endpoint);
+
+    std::cout.write(recv_buf.data(), len);
   }
   catch (std::exception& e)
   {
