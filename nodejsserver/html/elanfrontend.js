329f4bc0a314c321081e43f06bfccd5f98c0bf32
Jeremy Rode <jprode@gmail.com>
2019-01-11 02:41:51 +0000

    Initial Node JS Websockets working

---
+++ nodejsserver/html/elanfrontend.js
@@ -0,0 +1,49 @@
+$(function () {
+  "use strict";
+  // for better performance - to avoid searching in DOM
+  var status = $('#status');
+  // if user is running mozilla then use it's built-in WebSocket
+  window.WebSocket = window.WebSocket || window.MozWebSocket;
+  // if browser doesn't support WebSocket, just show
+  // some notification and exit
+  if (!window.WebSocket) {
+    content.html($('<p>',
+      { text:'Sorry, but your browser doesn\'t support WebSocket.'}
+    ));
+    return;
+  }
+  // open connection
+  var connection = new WebSocket('ws://172.16.17.204:1338');
+  connection.onopen = function () {
+    status.text('Ready');
+  };
+  connection.onerror = function (error) {
+    // just in there were some problems with connection...
+    content.html($('<p>', {
+      text: 'Sorry, but there\'s some problem with your '
+         + 'connection or the server is down.'
+    }));
+  };
+  // most important part - incoming messages
+  connection.onmessage = function (message) {
+      console.log('Got a message:', message);
+  };
+
+  document.getElementsByClassName("commandbutton").onclick = function(){
+	status.text('Got Click');
+  };
+
+  /**
+   * This method is optional. If the server wasn't able to
+   * respond to the in 3 seconds then show some error message 
+   * to notify the user that something is wrong.
+   */
+  setInterval(function() {
+    if (connection.readyState !== 1) {
+      status.text('Error');
+      input.attr('disabled', 'disabled').val(
+          'Unable to communicate with the WebSocket server.');
+    }
+  }, 3000);
+});
+
