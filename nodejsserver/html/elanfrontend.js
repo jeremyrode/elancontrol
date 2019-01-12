cd5bb7ca70b2ffca6856cb78e59f89fd3df5b3ca
Jeremy Rode <jprode@gmail.com>
2019-01-12 00:33:21 +0000

    Websockets working

--- nodejsserver/html/elanfrontend.js
+++ nodejsserver/html/elanfrontend.js
@@ -1,7 +1,9 @@
+
 $(function () {
   "use strict";
   // for better performance - to avoid searching in DOM
   var status = $('#status');
+  
   // if user is running mozilla then use it's built-in WebSocket
   window.WebSocket = window.WebSocket || window.MozWebSocket;
   // if browser doesn't support WebSocket, just show
@@ -12,6 +14,8 @@ $(function () {
     ));
     return;
   }
+
+
   // open connection
   var connection = new WebSocket('ws://172.16.17.204:1338');
   connection.onopen = function () {
@@ -28,10 +32,8 @@ $(function () {
   connection.onmessage = function (message) {
       console.log('Got a message:', message);
   };
+  //$(document).on('click', '.commandbutton', clickFun);
 
-  document.getElementsByClassName("commandbutton").onclick = function(){
-	status.text('Got Click');
-  };
 
   /**
    * This method is optional. If the server wasn't able to
@@ -41,9 +43,19 @@ $(function () {
   setInterval(function() {
     if (connection.readyState !== 1) {
       status.text('Error');
-      input.attr('disabled', 'disabled').val(
-          'Unable to communicate with the WebSocket server.');
+      console.log('Connection Error');
     }
   }, 3000);
 });
 
+  function clickFun(event){
+	  //if (connection.readyState == 1) {
+		  //connection.send('GotClick');
+		  //status.text('Got Click');
+	      console.log('Got Click');
+	 // }
+	 // else {
+	//	  console.log('Got Click, connection not ready');
+		  //status.text('Connection Error');
+	 // }
+  };
